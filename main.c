fdf4bf7264edc0e834ce5b1a0b8863d51a0a31ed
Ryan Tan <ryantan.mail@gmail.com>
2022-08-26 17:03:40 -0600

    pusha-T

--- main.c
+++ main.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
-int main(){ 
+int starting_screen(){
+	printf("LET'S GET READY TO BE GAMING\n");
+	return 0;
 
-	printf("Hello World\n");
+}
 
+int load_prompt(){
+	return 0;
+}
+
+
+int main(int argc, char* argv[]){ 
+	if (starting_screen() == 0){
+		load_prompt();
+		return 0;
+	}
 }
